e2e90e1f162c55cb0a2653aba3f7c2e5cbf8d011
Calmacil <togo.harrian@gmail.com>
2016-02-29 23:01:54 +0100

    Various things

--- src/Home.php
+++ src/Home.php
@@ -18,16 +18,34 @@ class Home extends \Calma\Mf\Controller
 
   public function index()
   {
+    $this->notice('This is the Home Page!');
     return 'text/html';
   }
 
   public function member()
   {
+    $this->notice('This is the members page!');
     return 'text/html';
   }
 
   public function protect()
   {
+    $this->notice('This is the admin page!');
     return 'text/html';
+  }
+  
+  public function login()
+  {
+    $this->notice('This is the login script!');
+    $username = $this->post('username');
+    $password = $this->post('password');
+    $this->info('Checking for login and password');
+    
+    if ($this->app['security']->login($username, $password)) {
+      $this->info('OK: User authentified, go on');
+      return $this->redirect('member');
+    }
+    $this->info('KO: User rejected, go back');
+    return $this->redirect('home');
   }
 }
